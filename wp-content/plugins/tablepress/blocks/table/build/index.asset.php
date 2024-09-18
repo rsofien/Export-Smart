e04aea86c0356f51ee4beab7798878dab5e24ae8
Sofiene Rhouma <sofienrhoumafreelance@gmail.com>
2024-09-18 21:52:51 +0100

    update

---
+++ wp-content/plugins/tablepress/blocks/table/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render', 'wp-shortcode'), 'version' => '3ab60a998362bdee359b');
